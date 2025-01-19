e99fe1374aefa5181472a1e6b7f57ef0d2e281ce
mate287 <mate.alfredo98@gmail.com>
2025-01-19 16:08:28 +0100

    first commit

---
+++ src/test/java/com/salle/RESTfulAPIApp/ResTfulApiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salle.RESTfulAPIApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTfulApiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
