1c4e7d8e9fc45d5f8f1ab91c0ebb52149b0ad713
Pol Piñol Castuera <pol.pinolcastuera@gmail.com>
2025-01-27 19:16:02 +0100

    Fix linter (#35)

--- src/main/java/com/salle/RESTfulAPIApp/Controller.java
+++ src/main/java/com/salle/RESTfulAPIApp/Controller.java
@@ -5,8 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
-  @GetMapping("/hello")
-  public String sayHello() {
-    return "Bye, World 6.0!";
-  }
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Bye, World 6.0!";
+    }
 }
+
